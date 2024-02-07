44e9041bf75aae4a4f6e8aaa41ee05cf4a6aa806
Mateus <mateusff2323@gmail.com>
2024-02-06 21:42:03 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from time import sleep
+from faker import Faker
+import mysql.connector
+
+bancoDeDados = mysql.connector.connect(
+    host="",
+    user="",
+    passwd="",
+    database=""
+)
+
+meuCursor = bancoDeDados.cursor()
+quantidadeDePessoas = 150
+counter = 0
+while (counter <= quantidadeDePessoas):
+    faker = Faker()
+
+    nome = faker.first_name()
+    nome = str(nome)
+    email = faker.email()
+    email = str(email)
+
+    dados = (nome, email)
+    query = "INSERT INTO clientes (nome, email) VALUES (%s, %s)"
+    meuCursor.execute(query, dados)
+    bancoDeDados.commit()
+    counter += 1
